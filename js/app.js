bb0bed0c33cd897871bfc6986694aca726050087
Raymundo Flores <60938574+raymundoflr@users.noreply.github.com>
2024-05-13 12:28:37 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+const nombre = prompt('Cual es tu nombre?');
+
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript`;
+
+const producto = 'Monitor 24 pulgadas';
